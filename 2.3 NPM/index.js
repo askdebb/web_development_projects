02982c1fece444bb14ffa2bbec38b89ac5ea70f8
Christopher Wiafe Debrah <cd.jobs.w@gmail.com>
2024-03-31 20:37:21 +0000

    added modules from npm

---
+++ 2.3 NPM/index.js
@@ -0,0 +1,11 @@
+// const generateName = require("sillyname");
+
+import superheroes from "superheroes";
+import generateName from "sillyname";
+
+
+const sillyname = generateName();
+
+console.log(`My super hero name is ${superheroes.random()}`);
+
+console.log(`My name is ${sillyname}`);
